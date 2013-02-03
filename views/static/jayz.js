62a07498d0993abd7662ad3b7c91a3c779b698fb
David Guo <david.guo@uwaterloo.ca>
2013-02-02 23:43:56 -0500

    Jay-Z

---
+++ views/static/jayz.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	$("#submit-challenge-button").click(function () {
+		$('#form').show();
+	});
+
+	$("#challenge-submit-button").click(function () {
+		$.ajax({
+			type: "POST",
+			url: "/index/",
+			data: { challenge: $('#challenge-input').val(),
+					unit: $('#unit-input').val(),
+					quant: $('input[@name="quant"]:checked').val(),
+					order: $('input[@name="order"]:checked').val()},
+			success: function(data) {
+				location.reload();
+			}
+		});	
+	});
+
+	$("#challenge-button").click(function () {
+		$.ajax({
+			type: "POST",
+			url: "/challenge/",
+			data: { },
+			success: function(data) {
+				location.reload();
+			}
+		});	
+	});
+});
