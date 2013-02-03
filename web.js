a7b750a2829bfa7d83ca2c577c93b19ab15d310b
rosepuppy <hezhenqian@gmail.com>
2013-02-03 07:35:30 -0500

    fix

--- web.js
+++ web.js
@@ -50,7 +50,7 @@ function render_page(req, res, pgPath, option) {
 }
 
 function handle_category_post_request(req, res) {
-  dbaccess.createChallenge(null, req.body.category, function(match)
+  dbaccess.createChallenge(null, req.body.category, function(err, match)
   {
     res.redirect('/category');
   });
