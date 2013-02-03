86d2f802d0aebb9cb517f5e32733c38393d7c3a0
Josh Dhaliwal <arreth@gmail.com>
2013-02-02 22:02:27 -0500

    trying something to maybe fix R10 error on heroku

--- web.js
+++ web.js
@@ -9,35 +9,6 @@ var path = require('path');
 
 var challenge_id = -1;
 
-Eventer = function(){
-  events.EventEmitter.call(this);
-  this.createChallenge = function( creator, inTitle, inType, inMinmax ){
-    this.emit('createChallenge',  creator, inTitle, inType, inMinmax );
-  }
-
-  this.createEntry = function(creator, inTitle, inChallenge, inMetric, inContent ){
-    this.emit('createEntry', creator, inTitle, inChallenge, inMetric, inContent );
-  }
- };
-
-util.inherits(Eventer, events.EventEmitter);
-
-Listener = function(){
-  this.createChallengeHandler =  function( creator, inTitle, inType, inMinmax ){
-    //console.log('works');
-    dbaccess.createChallenge( creator, inTitle, inType, inMinmax );
-  },
-  this.createEntryHandler = function(creator, inTitle, inChallenge, inMetric, inContent ){
-    dbaccess.createEntry(userId, Value, challengeName);
-  }
-};
-
-var eventer = new Eventer();
-var listener = new Listener(eventer);
-
-eventer.on('createChallenge',listener.createChallengeHandler);
-eventer.on('createEntry',listener.createEntryHandler);
-
 // create an express webserver
 var app = express.createServer(
   express.logger(),
@@ -78,6 +49,36 @@ app.dynamicHelpers({
     }
   },
 });
+
+Eventer = function(){
+  events.EventEmitter.call(this);
+  this.createChallenge = function( creator, inTitle, inType, inMinmax ){
+    this.emit('createChallenge',  creator, inTitle, inType, inMinmax );
+  }
+
+  this.createEntry = function(creator, inTitle, inChallenge, inMetric, inContent ){
+    this.emit('createEntry', creator, inTitle, inChallenge, inMetric, inContent );
+  }
+ };
+
+util.inherits(Eventer, events.EventEmitter);
+
+Listener = function(){
+  this.createChallengeHandler =  function( creator, inTitle, inType, inMinmax ){
+    //console.log('works');
+    dbaccess.createChallenge( creator, inTitle, inType, inMinmax );
+  },
+  this.createEntryHandler = function(creator, inTitle, inChallenge, inMetric, inContent ){
+    dbaccess.createEntry(userId, Value, challengeName);
+  }
+};
+
+var eventer = new Eventer();
+var listener = new Listener(eventer);
+
+eventer.on('createChallenge',listener.createChallengeHandler);
+eventer.on('createEntry',listener.createEntryHandler);
+
 
 function render_page(req, res, pgPath) {
   req.facebook.app(function(app) {
